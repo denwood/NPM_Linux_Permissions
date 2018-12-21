3582905a469894cd063f58c44a2afc893133dc79
denwood <KD.KouznetsovDenis@gmail.com>
2018-12-21 08:53:56 +0100

    Update npm_conf.sh

--- npm_conf.sh
+++ npm_conf.sh
@@ -5,6 +5,10 @@ npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 source ~/.profile
 
+echo 'export PATH=~/.npm-global/bin:$PATH' > ~/.bashrc
+echo 'export PATH=~/.npm-global/bin:$PATH' > ~/.zshrc
+
+
 # npm i -g node-red 
 # npm i -g nodemon
 
